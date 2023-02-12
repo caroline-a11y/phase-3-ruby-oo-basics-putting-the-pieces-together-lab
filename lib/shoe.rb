c980ada44ed5d29e8893e52bb449ddb56c333aeb
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-02-12 12:06:32 +0300

    completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class shoe
+    attr_reader :brand
+    attr_accessor :colour, :size, :material, :condition
+    def initialize(brand="Nike")
+        @brand = brand
+    end
+    def cobble
+        @condition ="new"
+        puts = "Your shoe is as good as new!"
+    end 
+end
